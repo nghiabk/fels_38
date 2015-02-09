081f7f054ad6d4ff443cab0693bcec432b991d4e
nghiabk <nghiabkthachthanh@gmail.com>
2015-02-09 15:47:18 +0700

    create follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
-
   root             'static_pages#home'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
-
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy' 
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships,       only: [:create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206082712) do
+ActiveRecord::Schema.define(version: 20150209080015) do
 
   create_table "answers", force: true do |t|
     t.string   "content"
@@ -48,6 +48,17 @@ ActiveRecord::Schema.define(version: 20150206082712) do
 
   add_index "lessons", ["category_id"], name: "index_lessons_on_category_id"
   add_index "lessons", ["user_id"], name: "index_lessons_on_user_id"
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "results", force: true do |t|
     t.integer  "lesson_id"
