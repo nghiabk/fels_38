1a655e394beb5f873c2b969d81716b64e11e3412
hung son <hungson91cb@gmail.com>
2015-02-11 10:30:10 +0700

    update profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ActiveRecord::Base
   has_many :lessons, dependent: :destroy
   has_many :learns, dependent: :destroy
 
+  attr_accessor :remember_token 
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX},
                                 uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, length: {minimum: 6}
+  validates :password, length: {minimum: 6}, allow_blank: true
   has_many :active_relationships, class_name:  'Relationship',
                                   foreign_key: 'follower_id',
                                   dependent:   :destroy
@@ -32,5 +34,29 @@ class User < ActiveRecord::Base
 
   def get_relationship? user
     self.active_relationships.find_by followed_id: user.id
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end  
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end  
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes(remember_digest: User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end 
+  
+  def forget
+    update_attributes(remember_digest: nil)
   end
 end
