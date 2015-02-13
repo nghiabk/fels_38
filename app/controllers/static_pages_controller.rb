749af2d10617f314c01a46df84df55e921ec7d24
nghiabk <nghiabkthachthanh@gmail.com>
2015-02-13 10:35:51 +0700

    create home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
+  include SessionsHelper
+  
   def home
+    if logged_in?
+      @feed_items = current_user.feed.paginate page: params[:page]
+    end
   end
-
-  def contact
-  end
+  
 end
